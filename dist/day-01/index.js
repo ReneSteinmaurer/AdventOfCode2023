56b17786800c63c059153f7dcb99825c78211eeb
Rene Steinmaurer <55176260+ReneSteinmaurer@users.noreply.github.com>
2023-12-02 18:34:41 +0100

    implemented day 1 and 2

---
+++ dist/day-01/index.js
@@ -0,0 +1,67 @@
+import * as fs from 'fs';
+const words = fs.readFileSync('src/day-01/input.txt', 'utf-8');
+const inputList = words.split('\r\n');
+const testList = [
+    'two1nine',
+    'eightwothree',
+    'abcone2threexyz',
+    'xtwone3four',
+    '4nineeightseven2',
+    'zoneight234',
+    '7pqrstsixteen'
+];
+let sumOfAllNumbers = 0;
+function getNumberSumFromString(str) {
+    let stringBuilder = '';
+    let elements = [];
+    for (const strElement of str) {
+        stringBuilder += strElement;
+        if (strElement.match(/\d+/)) {
+            elements.push(strElement);
+            stringBuilder = '';
+        }
+        else if (stringBuilder.includes("one")) {
+            elements.push("1");
+            stringBuilder = '';
+        }
+        else if (stringBuilder.includes("two")) {
+            elements.push("2");
+            stringBuilder = '';
+        }
+        else if (stringBuilder.includes("three")) {
+            elements.push("3");
+            stringBuilder = '';
+        }
+        else if (stringBuilder.includes("four")) {
+            elements.push("4");
+            stringBuilder = '';
+        }
+        else if (stringBuilder.includes("five")) {
+            elements.push("5");
+            stringBuilder = '';
+        }
+        else if (stringBuilder.includes("six")) {
+            elements.push("6");
+            stringBuilder = '';
+        }
+        else if (stringBuilder.includes("seven")) {
+            elements.push("7");
+            stringBuilder = '';
+        }
+        else if (stringBuilder.includes("eight")) {
+            elements.push("8");
+            stringBuilder = '';
+        }
+        else if (stringBuilder.includes("nine")) {
+            elements.push("9");
+            stringBuilder = '';
+        }
+    }
+    debugger;
+    return Number.parseInt(elements[0] + elements[elements.length - 1]);
+}
+for (const input of inputList) {
+    sumOfAllNumbers += getNumberSumFromString(input);
+}
+console.log(sumOfAllNumbers);
+//# sourceMappingURL=index.js.map
---
+++ dist/day-02/index.js
@@ -0,0 +1,103 @@
+import fs from "fs";
+function getGameValueFromString(str) {
+    let gameId = str.replace(':', '').split(' ')[1];
+    let values = str.substring(str.indexOf(':') + 1).split(';');
+    let cubeList = [];
+    let lastValue;
+    for (let i = 0; i < values.length; i++) {
+        let blue = 0;
+        let red = 0;
+        let green = 0;
+        let cubeValues = values[i].split(' ');
+        for (let j = 0; j < cubeValues.length; j++) {
+            lastValue = cubeValues[j - 1];
+            if (cubeValues[j].includes('blue')) {
+                blue += Number.parseInt(lastValue);
+            }
+            else if (cubeValues[j].includes('red')) {
+                red += Number.parseInt(lastValue);
+            }
+            else if (cubeValues[j].includes('green')) {
+                green += Number.parseInt(lastValue);
+            }
+        }
+        cubeList.push({ red: red, blue: blue, green: green });
+    }
+    return {
+        id: Number.parseInt(gameId),
+        cubes: cubeList
+    };
+}
+function getPossibleGames(games, blue, green, red) {
+    let possibleGames = [];
+    let isPossible = true;
+    for (const game of games) {
+        isPossible = true;
+        for (let i = 0; i < game.cubes.length; i++) {
+            if (game.cubes[i].blue > blue) {
+                isPossible = false;
+            }
+            else if (game.cubes[i].red > red) {
+                isPossible = false;
+            }
+            else if (game.cubes[i].green > green) {
+                isPossible = false;
+            }
+        }
+        if (isPossible) {
+            possibleGames.push(game);
+        }
+    }
+    return possibleGames;
+}
+function getSumOfIdsFromPossibleGames(games) {
+    let ids = 0;
+    for (const game of games) {
+        ids += game.id;
+    }
+    return ids;
+}
+function getResultOfCubesWithHighestRedGreenBlue(game) {
+    let maxRed = 0;
+    let maxGreen = 0;
+    let maxBlue = 0;
+    for (const cube of game.cubes) {
+        debugger;
+        if (cube.green > maxGreen) {
+            maxGreen = cube.green;
+        }
+        if (cube.blue > maxBlue) {
+            maxBlue = cube.blue;
+        }
+        if (cube.red > maxRed) {
+            maxRed = cube.red;
+        }
+    }
+    return (maxRed * maxBlue * maxGreen);
+}
+function getResultOfAllWithHighestRedGreenBlue(games) {
+    let result = 0;
+    for (const game of games) {
+        debugger;
+        result += getResultOfCubesWithHighestRedGreenBlue(game);
+    }
+    return result;
+}
+let test = [
+    'Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green',
+    'Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue',
+    'Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red',
+    'Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red',
+    'Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green',
+];
+const words = fs.readFileSync('src/day-02/input.txt', 'utf-8');
+const inputList = words.split('\r\n');
+let gameList = [];
+for (const inputListElement of inputList) {
+    gameList.push(getGameValueFromString(inputListElement));
+}
+let possibleGames = getPossibleGames(gameList, 14, 13, 12);
+console.log(possibleGames);
+console.log("Sum of possible games: ", getSumOfIdsFromPossibleGames(possibleGames));
+console.log(getResultOfAllWithHighestRedGreenBlue(gameList));
+//# sourceMappingURL=index.js.map
